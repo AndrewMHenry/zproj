ebda01f4e020af2009724f1365a7c1e03ae9bb36
Andrew Henry <ahenry3@umbc.edu>
2017-06-16 15:50:32 -0400

    Update zmake.py to reprint contents of zproj file.

--- zproj/zmake.py
+++ zproj/zmake.py
@@ -1,6 +1,9 @@
+from .zproj import compile_zproj_file, disassemble_zproj
+
 def main():
     """Execute zmake as on command line."""
     print('Hello from zmake.py:main!')
+    print(disassemble_zproj(compile_zproj_file('.zproj')))
     # import argparse
     # import sys
 
