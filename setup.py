3974ef83da4623da670e875cd98201eb0c8c31be
Andrew Henry <ahenry3@umbc.edu>
2017-06-30 08:58:42 -0400

    Update version to 1.0.8 and add url in setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='zproj',
+      version='1.0.8',
       packages=find_packages(),
       entry_points = {
           'console_scripts': [
@@ -11,5 +12,6 @@ setup(name='zproj',
       },
       author='Andrew Henry',
       author_email='andrewmichaelhenry@gmail.com',
+      url='https://github.com/AndrewMHenry/zproj',
       include_package_data=True,
       install_requires=['peeker'])
