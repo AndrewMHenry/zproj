3580bb78f5bf8fafd43d68aedc12859037fdda8d
Andrew Henry <ahenry3@umbc.edu>
2017-07-29 23:03:02 -0400

    Extend data.py.

--- zproj/data.py
+++ zproj/data.py
@@ -5,6 +5,12 @@ DIR = os.path.dirname(__file__)
 LIB_BASE = 'lib'
 LIB_DIR = os.path.join(DIR, LIB_BASE)
 
+
+def get_include_dir(assembler):
+    """Return path to includes for given assembler."""
+    return os.path.join(LIB_DIR, 'spasm')
+
+
 KEY_DIR = LIB_DIR
 KEY_BASE = '0104.key'
 KEY_FILE = os.path.join(KEY_DIR, KEY_BASE)
